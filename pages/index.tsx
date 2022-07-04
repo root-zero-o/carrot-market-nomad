0398185827c92fb6a0f45a34cab3127ccaf31345
RootZero <rootzero17@gmail.com>
2022-07-04 13:39:31 +0900

    tailwind css 연습2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,20 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+    <div className="bg-slate-400 xl:place-content-center py-20 px-10 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white flex flex-col justify-center p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$170</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Tooly Table</span>
-          <span className="font-semibold">$800</span>
-        </div>
+          <ul>
+            {[1,2,3,4,5].map(i => 
+              <div key={i} className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-50"> 
+                <span className="text-gray-500">Grey Chair</span>
+                <span className="font-semibold">$170</span>
+              </div>)}
+          </ul>
+          <ul>
+            {["a", "b", "c", ""].map((c, i) => <li className="bg-red-500 py-2 empty:hidden" key={i}>{c}</li>)}
+          </ul>
+
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$970</span>
@@ -27,8 +30,8 @@ const Home: NextPage = () => {
             Checkout
         </button>
       </div>
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
+        <div className="bg-blue-500 landscape:bg-teal-500 p-6 pb-14 xl:pb-64">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl p-6 bg-white relative -top-5">
@@ -37,7 +40,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-semibold">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full"/>
+            <div className="h-24 w-24 bg-zinc-300 rounded-full group-hover:bg-red-300 transition-colors"/>
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-semibold">$2,310</span>
@@ -49,7 +52,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl">
+      <div className="bg-white p-10 rounded-2xl shadow-xl lg:col-span-2 xl:col-span-1">
         <div className='flex mb-5 justify-between items-center'>
           <span>👈</span>
           <div className="space-x-3">
@@ -63,9 +66,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
-              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
-              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-</button>
@@ -79,8 +82,8 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
+    
   )
 }
 
