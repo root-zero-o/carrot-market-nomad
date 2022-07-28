3c8e57252c4200ba9e281f4e847edf5346d17e76
영근 <rootzero17@gmail.com>
2022-07-28 18:35:23 +0900

    feat: layout, topbar & navbar

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -1,30 +1,10 @@
 import type { NextPage } from "next";
 import Layout from "../../components/layout";
 
-const Upload: NextPage = () => {
+const Create: NextPage = () => {
   return (
-    <Layout canGoBack>
-      <div className="px-4 space-y-5">
-        <div>
-          <label className="w-full cursor-pointer flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500">
-            <svg
-              className="h-12 w-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-
-            <input type="file" className="hidden" />
-          </label>
-        </div>
+    <Layout title="라이브 시작하기" canGoBack>
+      <div className=" space-y-5 px-4">
         <div>
           <label
             className="mb-1 block text-sm font-medium text-gray-700"
@@ -43,13 +23,13 @@ const Upload: NextPage = () => {
         </div>
         <div>
           <label
-            htmlFor="price"
             className="mb-1 block text-sm font-medium text-gray-700"
+            htmlFor="price"
           >
             Price
           </label>
-          <div className="rounded-md relative flex items-center shadow-sm">
-            <div className="absolute left-0 pl-3 pointer-events-none flex items-center justify-center">
+          <div className="rounded-md relative flex  items-center shadow-sm">
+            <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
               <span className="text-gray-500 text-sm">$</span>
             </div>
             <input
@@ -58,7 +38,7 @@ const Upload: NextPage = () => {
               type="text"
               placeholder="0.00"
             />
-            <div className="absolute right-0 pointer-events-none pr-3 flex items-center ">
+            <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
               <span className="text-gray-500">USD</span>
             </div>
           </div>
@@ -68,22 +48,20 @@ const Upload: NextPage = () => {
             htmlFor="description"
             className="mb-1 block text-sm font-medium text-gray-700"
           >
-            {" "}
             Description
           </label>
-
           <textarea
             id="description"
-            className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+            className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
             rows={4}
           />
         </div>
         <button className=" w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-          Upload item
+          Go live
         </button>
       </div>
     </Layout>
   );
 };
 
-export default Upload;
+export default Create;
